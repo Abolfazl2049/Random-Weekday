73e0cc627378055c467adf692fb7d5a6411af848
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2025-02-11 10:47:12 +0330

    pages

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="">
+    <dev >
+      <div id="dice" class="dice-con">
+        <div class="zero">شنبه</div>
+        <div class="one">یکشنبه</div>
+        <div class="two">دوشنبه</div>
+        <div class="three">سه‌شنبه</div>
+        <div class="four">چهارشنبه</div>
+        <div class="five">پنجشنبه</div>
+        <div class="six">جمعه</div>
+      </div>
+    </dev>
+  </div>
+</template>
