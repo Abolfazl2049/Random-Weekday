74f4cfc40cab657ee9ccbde24734f8266f1ca2c2
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2025-02-15 18:47:20 +0330

    some transform

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
 <template>
-  <div class="h-screen flex items-center justify-center"></div>
+  <div class="h-screen flex items-center justify-center">
+    <div class="dice-con relative">
+      <div class="pentagon" />
+      <div class="side zero" />
+      <div class="side one" />
+      <div class="side two" />
+      <div class="side three" />
+      <div class="side four" />
+    </div>
+  </div>
 </template>
