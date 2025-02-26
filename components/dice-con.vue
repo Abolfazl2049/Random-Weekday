46fd6a41b278d1d0b473f91aa6d0128de7eb2e70
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2025-02-26 14:57:57 +0330

    dice count

--- components/dice-con.vue
+++ components/dice-con.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="dice-con relative top-[100px] right-20 z-10">
-    <div class="side one"><SideNumber> 1 </SideNumber></div>
-    <div class="side two"><SideNumber> 2 </SideNumber></div>
-    <div class="side three"><SideNumber> 3 </SideNumber></div>
-    <div class="side four"><SideNumber> 4 </SideNumber></div>
-    <div class="side five"><SideNumber> 5 </SideNumber></div>
-    <div class="six"><SideNumber class="!text-black translate-y-2"> 6 </SideNumber></div>
-    <div class="seven"><SideNumber class="translate-y-[10px] !text-black"> 7 </SideNumber></div>
+  <div class="z-10 relative">
+    <div class="dice-con">
+      <div class="side one"><SideNumber> 1 </SideNumber></div>
+      <div class="side two"><SideNumber> 2 </SideNumber></div>
+      <div class="side three"><SideNumber> 3 </SideNumber></div>
+      <div class="side four"><SideNumber> 4 </SideNumber></div>
+      <div class="side five"><SideNumber> 5 </SideNumber></div>
+      <div class="six"><SideNumber class="!text-black translate-y-2"> 6 </SideNumber></div>
+      <div class="seven"><SideNumber class="translate-y-[10px] !text-black"> 7 </SideNumber></div>
+    </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,30 @@
+<script setup lang="ts">
+let diceCount = ref(6);
+let rollDice = () => {
+  let day = new Date().getDay();
+  for (let i of Array(2)) console.log(Math.round(Math.random() * (6 - day) + day));
+};
+</script>
 <template>
-  <div class="h-screen flex items-center justify-center bg-teal-900">
-    <DiceCon />
+  <div class="h-screen flex items-center justify-center bg-teal-900 relative">
+    <div class="relative right-[550px]">
+      <DiceCon v-for="i in diceCount" :id="'dice-' + i.toString()" :style="`left : ${i * 140}px`" />
+    </div>
     <Floor />
+    <div class="absolute p-4 z-10 bottom-0 right-5 flex items-center gap-4">
+      <div class="flex bg-white p-4 rounded-lg">
+        <div>Dice Count :</div>
+        <select v-model="diceCount" class="">
+          <option :value="1">1</option>
+          <option :value="2">2</option>
+          <option :value="3">3</option>
+          <option :value="4">4</option>
+          <option :value="5">5</option>
+          <option :value="6">6</option>
+          <option :value="7">7</option>
+        </select>
+      </div>
+      <button @click="rollDice" class="bg-rose-700 p-4 rounded-lg">Roll Dice</button>
+    </div>
   </div>
 </template>
