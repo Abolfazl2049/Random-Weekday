6f850917ee58d330aaa61e9ab9a2575dc5a85ee2
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2025-03-01 17:43:43 +0330

    dice animation fully working

--- components/dice-con.vue
+++ components/dice-con.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
 let diceEl = ref<HTMLDivElement>();
-let animate = (diceS: number) => {
-  diceEl.value?.classList.add(`to-side-${diceS}`);
+let animatedTo = ref();
+let animate = (side: number) => {
+  if (diceEl.value) {
+    if (animatedTo.value !== undefined) diceEl.value.classList.remove(`to-side-${animatedTo.value}`);
+    animatedTo.value = side;
+
+    diceEl.value.style.animation = "none";
+    setTimeout(() => {
+      if (diceEl.value) {
+        diceEl.value.style.animation = "";
+        diceEl.value.classList.add(`to-side-${side}`);
+      }
+    }, 10);
+  }
 };
-defineExpose(animate);
+defineExpose({animate});
 </script>
 <template>
-  <div ref="diceEl" class="dice-con z-10 relative transition-all">
+  <div ref="diceEl" class="dice-con relative transition-all size-32 border border-red-600">
     <div class="side one"><SideNumber> 1 </SideNumber></div>
     <div class="side two"><SideNumber> 2 </SideNumber></div>
     <div class="side three"><SideNumber> 3 </SideNumber></div>
--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,21 @@ let diceCount = ref(2);
 let dice = ref();
 let rollDice = () => {
   let day = new Date().getDay();
+  day = 0;
   for (let i = 0; i < diceCount.value; i++) {
     let side = Math.round(Math.random() * (6 - day) + day);
-    dice.value[i](side + 1);
+    dice.value[i].animate(side + 1);
   }
 };
 </script>
 <template>
-  <div class="h-screen flex items-center justify-center bg-teal-900 relative">
-    <div class="right-[170px] bottom-32 relative"><DiceCon ref="dice" v-for="i in diceCount" :style="`left : ${i * 140}px`" /></div>
+  <div class="h-screen flex items-center justify-center bg-teal-900">
+    <div class="relative z-10 flex gap-8 bottom-[200px]"><DiceCon ref="dice" v-for="i in diceCount" /></div>
     <Floor />
     <div class="absolute p-4 z-10 bottom-0 right-5 flex items-center gap-4">
       <div class="flex bg-white p-4 rounded-lg">
         <div>Dice Count :</div>
-        <select v-model="diceCount" class="">
+        <select v-model="diceCount">
           <option :value="1">1</option>
           <option :value="2">2</option>
           <option :value="3">3</option>
